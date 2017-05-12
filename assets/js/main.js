6e9cec52436c966e52c38e60d87a8b6db8c7590f
Gabriela Bravo Faúndez <gbravo.visual@gmail.com>
2017-05-11 23:25:48 -0300

    Errores corregidos, resultado igual al original

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,6 @@ window.addEventListener("scroll", function(){
        document.getElementById("nav-header").classList.remove("header-in");
        document.getElementById("nav-header").style.opacity = 0;
     } else {
-        console.log("up");
         document.getElementById("nav-header").classList.add("header-in");
         document.getElementById("nav-header").classList.add("solid");
         document.getElementById("nav-header").style.opacity = 1;
